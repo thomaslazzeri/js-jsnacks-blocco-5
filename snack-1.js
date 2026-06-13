f63662c16ab3bd374c06c0eaba7553f30b92ba1f
thomaslazzeri <thomaslazzeri3@gmail.com>
2026-06-13 20:33:17 +0200

    done snack-1

--- snack-1.js
+++ snack-1.js
@@ -2,3 +2,12 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+names.forEach((el, i) => {
+    console.log(el, i);
+});
+
+
+
+
+
