8165e6e38a88f565b7c7774b8de94158b3c5c67c
thomaslazzeri <thomaslazzeri3@gmail.com>
2026-06-14 14:30:20 +0200

    done snack-8

--- snack-8.js
+++ snack-8.js
@@ -21,3 +21,8 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const studente = students.find(s => s.name === 'Marco Lanci');
+
+const classeTrovata = studente?.class;
+console.log(classeTrovata); 
