bb1ca066661e7521140e453cb18f7f4eacfdc97e
thomaslazzeri <thomaslazzeri3@gmail.com>
2026-06-14 13:56:00 +0200

    done snack-4

--- snack-4.js
+++ snack-4.js
@@ -7,3 +7,6 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const autori = posts.map(post => post.author);
+console.log(autori); 
